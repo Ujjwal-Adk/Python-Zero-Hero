4063dc8203be6236b36db6ffe8dffc00efc9ce71
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-09-24 14:51:07 -0600

    basics-datatypes

---
+++ 1-day.py
@@ -0,0 +1,40 @@
+# Lets start with the very starting print function 
+print("Hello , World !")
+print("Welcome to the Day 1 , Lets code with Ujjwal ")
+
+#  Understanding the variables 
+
+# So variable are something where the variables are stored 
+name = "Ujjwal" # Strings 
+age = 20 #numbers 
+print("My name is", name ,"and my age is",age)
+
+#Data types 
+
+#Strings, So the strings are always stored in "" and they are simply the texts 
+city ="Edmonton"
+message ="Hello, from Edmonton !"
+
+#Integers, Integers are the number that are stored in variable , in mathematical term its whole number 
+grades = 98
+money = 2000
+
+#Float , Float are simply the decimal numbers 
+height = 5.8
+weight = 67.9
+
+#Boolean , True = 1 , False = 0 
+is_student = True 
+is_married = False 
+
+# Printing the values of variables 
+print(city)
+print(grades)
+print(height)
+print(is_married)
+
+#printing the type of the data types example 
+print(type(city))
+print(type(grades))
+print(type(height))
+print(type(is_married))
