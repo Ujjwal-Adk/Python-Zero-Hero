7f575432f49a02e4e47357fb9193d67e46f002a1
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-09-25 22:26:57 -0600

    updated-division

--- 2-day.py
+++ 2-day.py
@@ -8,3 +8,13 @@ print("The name of the user is", name )
 # Lets ask them for the age 
 age = input("What is your age ?: ")
 print("This persons age is", age)
+
+# Now learning to make the calculations from the user inputs 
+
+number_1 = int(input("Enter the first number : "))
+number_2 = int(input("Enter the second number : "))
+
+divison = number_1 // number_2
+divison1 = number_1 / number_2
+print("Here is the division result : ", divison)
+print("The another way divison is", divison1)
