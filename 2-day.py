1124c61909715841f259cda1c78b44318f7f8f93
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-09-25 22:09:36 -0600

    taking-inputs

---
+++ 2-day.py
@@ -0,0 +1,10 @@
+# Welcome to day 2 , Here we will learn how to take inpuit from the user 
+
+# Lets ask user for their username 
+
+name = input("What is your name ?: ")
+print("The name of the user is", name )
+
+# Lets ask them for the age 
+age = input("What is your age ?: ")
+print("This persons age is", age)
