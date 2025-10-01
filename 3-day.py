0b24c65caaf4559c5093594692268c31bfb9dade
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-09-30 23:26:19 -0600

    Chnaging_variables_type

--- 3-day.py
+++ 3-day.py
@@ -15,6 +15,30 @@ print("Is a greater or equal to b ?", a>=b)
 
 print("Is a smaller or equal to b ?" , a<=b)
 
+# SO = is for assigning values and then == is for comparing values 
 
+# Now we will learn about changing the data types 
 
+text_number = "404"
+# So the above mumber is in text form right now I mwan string 
+print(type(text_number))
 
+# To change it to the Int or float we have to do this 
+real_number = int(text_number)
+# This is now integers
+print(type(real_number))
+
+real_number = float(text_number)
+# This is now float = decimal values 
+print(type(real_number))
+
+# Changing to String again 
+evo_text_number = str(real_number)
+print(type(evo_text_number))
+
+pie = 3.14
+changed_value_pie = int(pie)
+print(changed_value_pie)
+print(type(changed_value_pie))
+
+#SO things to remember is that you cant change the any names type of string to float and int and if there is decimal value in any string then we cant chnage that to string 
