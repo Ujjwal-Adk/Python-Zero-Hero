e92fa21d1e089b8fa24a4e062aa911065f8a37f6
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-10-01 00:16:26 -0600

    password-checker

---
+++ password-checker.py
@@ -0,0 +1,13 @@
+# Here we will make the password checker using the python
+
+password = ""
+
+while password != "pythonking":
+    password = input("Please enter your password to continue :")
+
+    if password == "pythonking" :
+        print("Access Granted !")
+    else:
+        print("Password Wrong ! Please try Again.")
+
+
