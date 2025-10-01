73ec12d122fa2292fbe1cca704ca5b62860df369
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-09-30 23:08:06 -0600

    comparing-Operators

---
+++ 3-day.py
@@ -0,0 +1,20 @@
+# WElcome to day 3 here we will learn to compare the values 
+
+a = 10 
+b = 5
+
+print("Is a equal to b?", a==b)
+
+print("Is a not equal to b?", a!=b)
+
+print("Is a greater than b", a>b)
+
+print("Is a smaller than b", a<b)
+
+print("Is a greater or equal to b ?", a>=b)
+
+print("Is a smaller or equal to b ?" , a<=b)
+
+
+
+
