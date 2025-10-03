bfd2fbc4c449148fdbfca2cf760d0e6abc4a4afb
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-10-02 21:58:42 -0600

    for_loop

---
+++ 6-day.py
@@ -0,0 +1,6 @@
+# Welcome to day 6 lets learn about the for loops today 
+
+for i in range(5): # so a little bit twist is if range is till 5 then it will only print upto 5 
+    print(i)
+
+print("Loop Sucessfully executed !! ")
