44e4eff40571a27cff645f7edad5df1995ea9c3a
Ujjwal-Adk <ujjwal.official010@gmail.com>
2025-10-03 08:26:23 -0600

    fixed_bugs

--- calculator.py
+++ calculator.py
@@ -1,55 +1,67 @@
-# Requirements:
-# - Ask user for two numbers
-# - Ask which operation (+, -, *, /)
-# - Show the result
-# - Use if/elif/else to handle different operations
-# - Use a while loop to keep asking "Do you want to calculate again?"
+# Simple Calculator with Repeat Option
 
-num_1 = int(input("Enter the first number : "))
-num_2 = int(input("Enter the second number : "))
+# Start the while loop
+continue_calculating = "yes"
 
-operator = input("Enter the operator : ")
-
-if operator == "+" :
-    result = num_1 + num_2
-
-elif operator == "-":
-    desicion = input("Enter from which number you want to subtarct bigger or smaller :")
-    if desicion == "smaller":
-        if num_1<num_2:
-            result = num_1 - num_2
-        else:
-            result = num_2 - num_1
+while continue_calculating == "yes":
     
-    elif desicion == "bigger":
-        if num_1>num_2:
-            result = num_1 - num_2
+    num_1 = int(input("Enter the first number: "))
+    num_2 = int(input("Enter the second number: "))
+    
+    operator = input("Enter the operator (+, -, *, /): ")
+    
+    if operator == "+":
+        result = num_1 + num_2
+    
+    elif operator == "-":
+        decision = input("Subtract from bigger or smaller number? ")
+        if decision == "smaller":
+            if num_1 < num_2:
+                result = num_1 - num_2
+            else:
+                result = num_2 - num_1
+        
+        elif decision == "bigger":
+            if num_1 > num_2:
+                result = num_1 - num_2
+            else:
+                result = num_2 - num_1
+        
         else:
-            result = num_2 - num_1
+            print("Invalid command!")
+            result = "Error"
+    
+    elif operator == "*":
+        result = num_1 * num_2
+    
+    elif operator == "/":
+        decision = input("Divide bigger by smaller or smaller by bigger? ")
+        if decision == "smaller":
+            if num_1 < num_2:
+                result = num_2 / num_1
+            else:
+                result = num_1 / num_2
+        
+        elif decision == "bigger":  # ✅ Fixed: was 'if', now 'elif'
+            if num_1 > num_2:
+                result = num_2 / num_1
+            else:
+                result = num_1 / num_2
+        
+        else:
+            print("Invalid command!")
+            result = "Error"
     
     else:
-        print("Invalid Command ! , Please Press Q to request for help !")
-
-elif operator == "*":
-    result = num_1 * num_2
-
-elif operator == "/":
-    desicion = input("You want to divide from smaller number or the bigger number ?") 
-    if desicion == "smaller":
-        if num_1<num_2:
-            result = num_2 / num_1
-        
-        else:
-            result = num_1 / num_2
-        
+        print("Invalid operator!")
+        result = "Error"
     
-    if desicion == "bigger":
-        if num_1>num_2:
-            result = num_2 / num_1
-        
-        else:
-            result = num_1 / num_2
-    else:
-        print("Invalid Command ! , Please Press Q to request for help !")
+    # Show result
+    print("Result:", result)
+    print("---")
+    
+    # Ask if user wants to continue
+    continue_calculating = input("Do you want to calculate again? (yes/no): ")
+    continue_calculating = continue_calculating.lower()  # Makes "Yes", "YES" work too
 
-print("Here is the result : ", result)
+print("Thanks for using the calculator!")
